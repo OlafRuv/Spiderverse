a2d754abab08326ac075df5ff77d95c22c951cf2
Olaf <olaf.ruvag@gmail.com>
2022-05-11 13:14:12 -0500

    Escribimos la prueba para el test de spiderman

--- test/sipderman.test.js
+++ test/sipderman.test.js
@@ -1,6 +1,19 @@
+const Spiderman = require('./../app/spiderman.js')
+
 describe("Test Suite Dummy Description", ()=>{
-    test("Cade dummy", () => {
+    test("Case dummy", () => {
         const resultofsomething = 1 + 2
         expect(resultofsomething).toBe(3)
+    })
+})
+
+describe("Unit Test for SpiderMan Class", () => {
+    test("1) Create an spiderman object", () => {
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony")
     })
 })
